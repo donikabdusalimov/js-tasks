0532fc9a4fc726310599c906b9f92737468716a7
Donik <126549201+donikabdusalimov@users.noreply.github.com>
2023-03-31 15:10:29 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// // task № 1
+// let ask = +prompt('Кого хотите удалить по индексу?').trim()
+// let arr = ['Aleksey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica']
+// if (ask === 0) {
+//     arr.splice(0, 1)
+// } else if (ask === 1) {
+//     arr.splice(1, 1)
+// } else if (ask === 2) {
+//     arr.splice(2, 1)
+// } else if (ask === 3) {
+//     arr.splice(3, 1)
+// } else if (ask === 4) {
+//     arr.splice(4, 1)
+// }
+// console.log(arr);
+
+
+// // task № 2
+// let ask2 = prompt('Кого хотите удалить по имени?').toLowerCase().trim()
+// let arr2 = ['aleksey', 'kartoshka', 'margarita', 'morgenshtern', 'monica']
+// if (ask2 === 'aleksey') {
+//     arr2.splice(0, 1)
+// } else if (ask2 === 'kartoshka') {
+//     arr2.splice(1, 1)
+// } else if (ask2 === 'margarita') {
+//     arr2.splice(2, 1)
+// } else if (ask2 === 'morgenshtern') {
+//     arr2.splice(3, 1)
+// } else if (ask2 === 'monica') {
+//     arr2.splice(4, 1)
+// }
+// console.log(arr2);
+
+
